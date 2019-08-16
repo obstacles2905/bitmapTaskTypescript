db24b1fab96ca9b9548ba807ecf6a8d55cfb1fe4
nikitapismenniy <nikita.pismenniy@together.com>
2019-08-16 09:40:52 +0300

    tslint fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, OnApplicationBootstrap} from '@nestjs/common';
+import {Controller, Get} from "@nestjs/common";
 import * as readline from "readline";
 
 interface IBitMap {
